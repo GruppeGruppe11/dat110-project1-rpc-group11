593b9aa9679bca9b17722cb2688b34ba95d85e9a
Bjørn-Sigurd <89257693+h599026@users.noreply.github.com>
2023-02-02 11:21:29 +0100

    Task 1: MessageUtils.java Encapsulate and Decapsulate.

--- src/main/java/no/hvl/dat110/messaging/MessageUtils.java
+++ src/main/java/no/hvl/dat110/messaging/MessageUtils.java
@@ -1,9 +1,5 @@
 package no.hvl.dat110.messaging;
 
-import java.util.Arrays;
-
-import no.hvl.dat110.TODO;
-
 public class MessageUtils {
 
 	public static final int SEGMENTSIZE = 128;
@@ -14,16 +10,17 @@ public class MessageUtils {
 	public static byte[] encapsulate(Message message) {
 		
 		byte[] segment = null;
-		byte[] data;
+		byte[] data = message.getData();
 		
 		// TODO - START
 		
 		// encapulate/encode the payload data of the message and form a segment
 		// according to the segment format for the messaging layer
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-			
+		segment = new byte[128];
+		final Integer LENGTH = data.length;
+		segment[0] = LENGTH.byteValue();
+		System.arraycopy(data, 0, segment, 1, LENGTH);
+
 		// TODO - END
 		return segment;
 		
@@ -35,9 +32,11 @@ public class MessageUtils {
 		
 		// TODO - START
 		// decapsulate segment and put received payload data into a message
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
+
+		final int LENGTH = segment[0];
+		byte[] data = new byte[LENGTH];
+		System.arraycopy(segment, 1, data, 0, LENGTH);
+		message = new Message(data);
 		
 		// TODO - END
 		
